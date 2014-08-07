2be9490d6f0d0e1bee41bbcad18552d610013466
Victor Kondratyuk <vkondratuk@gmail.com>
2014-08-08 02:35:19 +0400

    Get patching assembly path from project

--- Sinject.Patcher/PatchAssembliesTask.cs
+++ Sinject.Patcher/PatchAssembliesTask.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Build.Framework;
+﻿using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -16,8 +18,20 @@ namespace Sinject.Patcher
         {
             try
             {
-                Patcher.ProcessPatchesFile(this.PatchFilePath);
-                this.Log.LogMessage("Patched from file '{0}' processed", this.PatchFilePath);
+                var assemblies = PatchesFile.Load(this.PatchFilePath);
+                foreach (var assembly in assemblies)
+                {
+                    var assemblyPath = GetDependentAssemblyPath(assembly.Name);
+                    var patcher = new Patcher(assemblyPath);
+                    foreach (var type in assembly.Types)
+                    {
+                        foreach (var method in type.Methods)
+                        {
+                            patcher.Patch(type.Name, method);
+                        }
+                    }
+                    patcher.Save(assemblyPath);
+                }
                 return true;
             }
             catch (Exception ex)
@@ -25,6 +39,23 @@ namespace Sinject.Patcher
                 this.Log.LogErrorFromException(ex);
                 return false;
             }
+        }
+
+        private string GetDependentAssemblyPath(string assemblyName)
+        {
+            var project = new Project(this.BuildEngine.ProjectFileOfTaskNode);
+            var items = project.GetItems("ProjectReference");
+            foreach (var item in items)
+            {
+                var refProject = new Project(item.EvaluatedInclude);
+                if (refProject.GetPropertyValue("AssemblyName") == assemblyName)
+                {
+                    return Path.Combine(
+                        refProject.GetPropertyValue("TargetDir"),
+                        refProject.GetPropertyValue("TargetName") + refProject.GetPropertyValue("TargetExt"));
+                }
+            }
+            throw new Exception("Invalid assembly name");
         }
     }
 }
---
+++ Sinject.Patcher/PatchesFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Sinject.Patcher
+{
+    public class PatchedAssembly
+    {
+        public string Name { get; private set; }
+
+        public ICollection<PatchedType> Types { get; private set; }
+
+        public PatchedAssembly(string name, ICollection<PatchedType> types)
+        {
+            this.Name = name;
+            this.Types = types;
+        }
+    }
+
+    public class PatchedType
+    {
+        public string Name { get; private set; }
+
+        public ICollection<string> Methods { get; private set; }
+
+        public PatchedType(string name, ICollection<string> methods)
+        {
+            this.Name = name;
+            this.Methods = methods;
+        }
+    }
+
+    public static class PatchesFile
+    {
+        public static ICollection<PatchedAssembly> Load(string filePath)
+        {
+            var root = XElement.Load(filePath);
+            var result = new List<PatchedAssembly>();
+            foreach (var assembly in root.Elements("assembly"))
+            {
+                var assemblyName = assembly.Attribute("name").Value;
+                var types = new List<PatchedType>();
+                foreach (var type in assembly.Elements("type"))
+                {
+                    var typeName = type.Attribute("name").Value;
+                    var methods = type.Value.Split(',').Select(token => token.Trim()).ToList();
+                    types.Add(new PatchedType(typeName, methods));
+                }
+                result.Add(new PatchedAssembly(assemblyName, types));
+            }
+            return result;
+        }
+    }
+}
