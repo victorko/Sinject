fe7763ec30a93f45c44700a01f40c15f73661937
Victor Kondratyuk <vkondratuk@gmail.com>
2014-08-08 08:55:35 +0400

    Get patching assembly from project
    Process file reference before (that was done) processing project
    reference

--- Sinject.Patcher/PatchAssembliesTask.cs
+++ Sinject.Patcher/PatchAssembliesTask.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Sinject.Patcher
@@ -44,12 +45,26 @@ namespace Sinject.Patcher
         private string GetDependentAssemblyPath(string assemblyName)
         {
             var project = new Project(this.BuildEngine.ProjectFileOfTaskNode);
-            var items = project.GetItems("ProjectReference");
+
+            // Simple assembly reference
+            var items = project.GetItems("Reference");
+            foreach (var item in items)
+            {
+                var assemblyFullName = new AssemblyName(item.EvaluatedInclude);
+                if (assemblyFullName.Name == assemblyName)
+                {
+                    return item.GetMetadataValue("HintPath"); //TODO: Check if exists and product nice exception
+                }
+            }
+
+            // Project reference
+            items = project.GetItems("ProjectReference");
             foreach (var item in items)
             {
                 var refProject = new Project(item.EvaluatedInclude);
                 if (refProject.GetPropertyValue("AssemblyName") == assemblyName)
                 {
+                    // http://social.msdn.microsoft.com/Forums/vstudio/en-US/d3c6e2de-1e87-49c2-b059-df074868e315/msbuild-targetpath?forum=msbuild
                     return Path.Combine(
                         refProject.GetPropertyValue("TargetDir"),
                         refProject.GetPropertyValue("TargetName") + refProject.GetPropertyValue("TargetExt"));
